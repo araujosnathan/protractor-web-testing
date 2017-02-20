04c795fc95b09c8ab671c5314d1833043c4bbf63
Nathanael Araujo <Nathanael.Araujo@CSSP-571.local>
2017-02-20 14:33:45 -0300

    Updating login test

--- tests/login.js
+++ tests/login.js
@@ -11,17 +11,17 @@ describe("Login LinkedIn\n", function(){
   });
 
   it("It is not possible to do login with invalid email", function(){
-    login_linkedin.login('invalid_email@gmail.com', 'stacknowovrflow7');
+    login_linkedin.login('invalid_email@gmail.com', 'valid_pass');
     expect(element(by.id('session_key-login-error')).isDisplayed()).toBeTruthy();
   });
 
   it("It is not possible to do login with invalid password", function(){
-    login_linkedin.login('nathanael.araujos@gmail.com', 'invalid_password');
+    login_linkedin.login('valid_email@gmail.com', 'invalid_password');
     expect(element(by.id('session_password-login-error')).isDisplayed()).toBeTruthy();
   });
 
   it("It is possible to do login with valid account", function(){
-    login_linkedin.login('nathanael.araujos@gmail.com', 'overflowdefault7');
+    login_linkedin.login('valid_email@gmail.com', 'valid_pass');
     expect(element(by.id('feed-tab-icon')).isDisplayed()).toBeTruthy();
   });
 
