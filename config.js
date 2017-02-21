6047a9259b7ef84a8d76eb537366c04bc8e371e7
Nathanael Araujo <Nathanael.Araujo@CSSP-571.local>
2017-02-21 09:26:11 -0300

    Adding multiCapabilities in config file
    Signed-off-by: Nathanael Araujo <Nathanael.Araujo@CSSP-571.local>

--- config.js
+++ config.js
@@ -5,5 +5,18 @@ exports.config = {
   suites: {
     login: './tests/login.js',
   },
+
+  // Choose browser for executing the tests
+  // capabilities: {
+  //   'browserName': 'firefox'
+  // }
+
+  // Choose browsers that will run the tests in parallel
+  // multiCapabilities: [{
+  //   'browserName': 'chrome'
+  // }, {
+  //   'browserName': 'firefox',
+  // }]
+
   //specs: ['./tests/login.js'],
 }
