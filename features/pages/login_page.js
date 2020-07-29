51bbdf847f9e4bb78e3b18ddc5bb02870b3551d6
Nathanael Araújo <nathanaelaraujo@Nathanaels-MacBook-Pro.local>
2020-07-29 14:34:46 +0100

    Update locators

--- features/pages/login_page.js
+++ features/pages/login_page.js
@@ -5,11 +5,11 @@ let expect = chai.expect;
 
 var LoginPage = function(){
 
-  var email                         = element(by.id('login-email')),
-      password                      = element(by.id('login-password')),
-      login_button                  = element(by.id('login-submit')),
-      session_key_login_error       = element(by.id('session_key-login-error')),
-      session_password_login_error  = element(by.id('session_password-login-error')),
+  var email                         = element(by.id('session_key')),
+      password                      = element(by.id('session_password')),
+      login_button                  = element(by.css('.sign-in-form__submit-button')),
+      session_key_login_error       = element(by.id('error-for-username')),
+      session_password_login_error  = element(by.id('error-for-password')),
       feed_tab_icon                 = element(by.id('feed-tab-icon'))
 
   this.visit = function(url) {
@@ -23,7 +23,7 @@ var LoginPage = function(){
       login_button.click();
   };
 
-  this.checkUserError = function(callback) {
+  this.checkUserError = function(callback) { 
     expect(session_key_login_error.isDisplayed()).to.eventually.to.exist.and.notify(callback);
   };
 
